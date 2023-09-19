c432978e2b02573021977c501c88c3253f82d12f
youngho3313 <144213471+youngho3313@users.noreply.github.com>
2023-09-19 12:36:07 +0900

    Update MemberDeleteController.java

--- ApartTogether_01/src/main/java/com/apartogether/controller/member/MemberDeleteController.java
+++ ApartTogether_01/src/main/java/com/apartogether/controller/member/MemberDeleteController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.apartogether.controller.SuperClass;
 import com.apartogether.model.dao.MemberDao;
 
-// 작성중입니다.
 public class MemberDeleteController extends SuperClass {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -16,7 +15,7 @@ public class MemberDeleteController extends SuperClass {
 		int cnt = -1;
 		
 		try {
-			cnt = dao.deleteData(id) ; // MemberDao.deleteData 작성해야됨
+			cnt = dao.deleteData(id) ; // MemberDao.deleteData 
 			super.session.invalidate(); // 세션에 저장된 로그인 정보를 삭제한다.
 			new MemberLoginController().doGet(request, response); // 로그인페이지로 이동한다.
 		} catch (Exception e) {
